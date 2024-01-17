c1d5868f97dbb9621b31a44f6440991ec8713449
i-tereshchenko <i.tereschenko@weplay.tv>
2024-01-17 21:34:10 +0200

    - print block

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,12 @@ import scroll from "./scroll";
 import menu from "./menu"
 import animation from "./animation";
 import header from "./header";
+import pdf from "./pdf";
 
 window.addEventListener('DOMContentLoaded', () => {
   scroll()
   menu()
   animation()
   header()
+  pdf()
 })
---
+++ src/js/pdf.js
@@ -0,0 +1,8 @@
+import html2pdf from 'html2pdf.js'
+
+const pdf = () => {
+  const element = document.getElementById('print')
+  // html2pdf().from(element).save()
+};
+
+export default pdf;
